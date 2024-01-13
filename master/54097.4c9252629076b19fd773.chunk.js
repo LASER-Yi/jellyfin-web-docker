a70e612ffd76e475b9c90cefa72f98dcf6ba8a06
LASER-Yi <LASER-Yi@users.noreply.github.com>
2024-01-13 01:26:44 +0000

    Deploying to build from @ LASER-Yi/jellyfin-web-statics@2b19a26bb8c64aa50c6e05e4b8b832e293c50970 🚀

---
+++ master/54097.4c9252629076b19fd773.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[54097],{99703:function(e,n,u){u.r(n),u.d(n,{default:function(){return t}});var r=u(10005);function t(e,n,u){var t,i=this;i.renderTab=function(){t||(t=new r.Z({element:u,serverId:ApiClient.serverId()}))},i.onShow=function(){t&&t.resume()},i.onHide=function(){t&&t.pause()}}}}]);
